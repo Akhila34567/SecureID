918177012780b02d67fec296ea0039bdc0792a81
Akhila <akhilakaratlapally@gmail.com>
2026-08-25 16:52:19 +0530

    Fix homepage route

--- server/server.js
+++ server/server.js
@@ -70,6 +70,14 @@ app.use(
     )
 );
 
+app.get("/", (req, res) => {
+    res.sendFile(
+        path.join(
+            __dirname,
+            "../public/index.html"
+        )
+    );
+});
 
 // =====================================================
 // PASSWORD VALIDATION
